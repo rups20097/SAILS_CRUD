ec652cbae9bd9dcfc912cf121eccafa1f3c70e35
Rupam Basak <rups20097@gmail.com>
2018-08-06 10:59:17 +0530

    First Commit

---
+++ api/controllers/CompanyController.js
@@ -0,0 +1,90 @@
+/**
+ * CompanyController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+
+
+//fetchting indivisual data
+  async create(req,res){
+    //fetching whole data
+    try{
+      let params = req.allParams();
+      if(!params.name && !params.phone){
+        return res.badRequest({err: 'You missed either name or phone_number!'});
+      }
+      const results = await Company.create({
+        name: params.name,
+        phone: params.phone,
+        city: params.city
+
+        });
+        return res.ok(results);
+    }
+    catch(err){
+      res.serverError(err);
+    }
+  },
+
+
+//getting any  perticular object by id
+  async findOne(req,res){
+      try{
+        const admi = await Company.findOne(req.params.id);
+        return res.ok(admi);
+      }
+      catch(err){
+        res.serverError(err);
+      }
+  },
+
+//getting all the objects created in database
+  async findAll(req,res){
+      try{
+        const results = await Company.find();
+        return res.ok(results);
+      }
+      catch(err){
+        return res.serverError(err);
+      }
+  },
+
+//updating an boject with some attributes
+  async update(req,res){
+    try{
+      let params = req.allParams();
+      let attributes = {};
+
+      if(params.name){
+        attributes.name = params.name;
+      }
+      if(params.city){
+        attributes.city = params.city;
+      }
+      if(params.phone){
+        attributes.phone = params.phone;
+      }
+
+      const results = await Company.update({id: req.params.id}, attributes);
+      return res.ok(results);
+    }
+    catch(err){
+    return res.serverError(err);
+  }
+  },
+
+//dwleting an object
+  async delete(req,res){
+    try{
+      const res = await Company.destroy(req.params.id);
+      return res.ok(res);
+    }
+    catch(err){
+      return res.serverError(err);
+    }
+  },
+
+};
---
+++ config/routes.js
@@ -0,0 +1,50 @@
+/**
+ * Route Mappings
+ * (sails.config.routes)
+ *
+ * Your routes tell Sails what to do each time it receives a request.
+ *
+ * For more information on configuring custom routes, check out:
+ * https://sailsjs.com/anatomy/config/routes-js
+ */
+
+module.exports.routes = {
+
+  // '/': {
+  //     view: 'pages/hompage',
+  //   }
+  'POST /companies': 'CompanyController.create',
+  'GET /companies/:id': 'CompanyController.findOne',
+  'GET /companies': 'CompanyController.findAll',
+  'PATCH /companies/:id': 'CompanyController.update',
+  'DELETE /companies/:id': 'CompanyController.delete',
+
+  /***************************************************************************
+  *                                                                          *
+  * More custom routes here...                                               *
+  * (See https://sailsjs.com/config/routes for examples.)                    *
+  *                                                                          *
+  * If a request to a URL doesn't match any of the routes in this file, it   *
+  * is matched against "shadow routes" (e.g. blueprint routes).  If it does  *
+  * not match any of those, it is matched against static assets.             *
+  *                                                                          *
+  ***************************************************************************/
+
+
+  //  ╔═╗╔═╗╦  ╔═╗╔╗╔╔╦╗╔═╗╔═╗╦╔╗╔╔╦╗╔═╗
+  //  ╠═╣╠═╝║  ║╣ ║║║ ║║╠═╝║ ║║║║║ ║ ╚═╗
+  //  ╩ ╩╩  ╩  ╚═╝╝╚╝═╩╝╩  ╚═╝╩╝╚╝ ╩ ╚═╝
+
+
+
+  //  ╦ ╦╔═╗╔╗ ╦ ╦╔═╗╔═╗╦╔═╔═╗
+  //  ║║║║╣ ╠╩╗╠═╣║ ║║ ║╠╩╗╚═╗
+  //  ╚╩╝╚═╝╚═╝╩ ╩╚═╝╚═╝╩ ╩╚═╝
+
+
+  //  ╔╦╗╦╔═╗╔═╗
+  //  ║║║║╚═╗║
+  //  ╩ ╩╩╚═╝╚═╝
+
+
+};
